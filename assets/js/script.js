7d9047b84419a64e0ce4e6fc1a05c95ebf0612a7
DHDH <damon.ha6@gmail.com>
2021-07-04 20:16:54 -0400

    add ability to add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
 // Create your HTML Page via DOM Methods here!
-
 var body = document.body;
 
+var buttonEl = document.querySelector("#save-task"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
+
+var createTaskHandler = function() { 
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
+
+buttonEl.addEventListener("click", createTaskHandler);
+  
 // TODO: Add a centered h1
 
 // TODO: Add a centered h2
